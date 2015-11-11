c2d6d0f820467db8e436629842699c3fea909e7f
Morten Høfft <mhoefft@gbif.org>
2015-11-11 16:30:26 +0100

    styling and templates

---
+++ content/01_about/dk.md
@@ -0,0 +1,4 @@
+---
+title: Omkring
+type: menu only
+---
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,10 +1,22 @@
 //just for testing css for now
 $('.navigation-main>ul>li>ul>li>a').click(function () {
-    $(this).parent().toggleClass('active');
+    $(this).parent().toggleClass('isActive');
 });
 
 $('.toggle.toggle-nav').click(function () {
     $('.site-navigation').toggleClass('toggle');
-    $(this).toggleClass('active');
+    $(this).toggleClass('isActive');
     $('#main').toggleClass('toggle');
+    $('body').toggleClass('hasOverlay');
+});
+
+$('.overlay').on("click", function () {
+    $('.site-navigation').removeClass('toggle');
+    $('.toggle.toggle-nav').removeClass('isActive');
+    $('#main').removeClass('toggle');
+    $('body').removeClass('hasOverlay');
+});
+
+$('.languageSelector').click(function () {
+    $(this).toggleClass('isExpanded');
 });
